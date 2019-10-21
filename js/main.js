59e13ae6087c395038bee34f6ae29b1595843a8d
William Aleshire <Mine@Williams-iMac.domain>
2019-10-21 16:46:21 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
 console.log("Up and running!");
-const myCards = ["queen", "queen", "king", "king"];
+const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
-const cardOne = myCards[0];
+
+var checkForMatch = function () {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+      } else {
+        console.log("Sorry, try again.");
+      }
+}
+/*const cardOne = cards[0];
 cardsInPlay.push['cardOne']
 console.log("User flipped queen");
-const cardTwo = myCards[2];
+const cardTwo = cards[2];
 cardsInPlay.push['cardTwo']
-console.log("User flipped king");
+console.log("User flipped king");*/
 
-if (cardsInPlay.length === 2) {
+var flipCard = function (cardId) {
+
+/*if (cardsInPlay.length === 2) {
     result = "match";
   } else if (cardsInPlay[0] === cardsInPlay[2]) {
     result = "you found a match";
@@ -16,4 +26,10 @@ if (cardsInPlay.length === 2) {
   } else (cardsInPlay[0] !== cardsInPlay[2]);{
     result = "sorry, try again";
     alert("Sorry, try again.");
-  }
+  }*/
+checkForMatch();
+  console.log("User flipped" + cards[cardId]);
+  cards[cardId].push[cardsInPlay];
+};
+flipCard(2);
+flipCard(0);
