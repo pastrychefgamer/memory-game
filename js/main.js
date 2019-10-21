90bc5b4faf5fe78a539aa399d0753773adfe4219
William Aleshire <Mine@Williams-iMac.domain>
2019-10-21 15:52:20 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+const myCards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+const cardOne = myCards[0];
+cardsInPlay.push['cardOne']
+console.log("User flipped queen");
+const cardTwo = myCards[2];
+cardsInPlay.push['cardTwo']
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+    result = "match";
+  } else if (cardsInPlay[0] === cardsInPlay[2]) {
+    result = "you found a match";
+    alert("You found a match!");
+  } else (cardsInPlay[0] !== cardsInPlay[2]);{
+    result = "sorry, try again";
+    alert("Sorry, try again.");
+  }
