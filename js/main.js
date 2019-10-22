df294f955daf6cceb0a26ba2555fd8cda4a0a8b5
William Aleshire <Mine@Williams-iMac.domain>
2019-10-21 17:08:28 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 console.log("Up and running!");
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+    {
+        rank: "quee",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
 const cardsInPlay = [];
 
 var checkForMatch = function () {
@@ -28,8 +49,9 @@ var flipCard = function (cardId) {
     alert("Sorry, try again.");
   }*/
 checkForMatch();
-  console.log("User flipped" + cards[cardId]);
-  cards[cardId].push[cardsInPlay];
+  console.log("User flipped" + cards[cardId].rank);
+  cardsInPlay.push(cards["queen"].rank);
+  cardsInPlay.push(cards["king"].rank);
 };
 flipCard(2);
 flipCard(0);
